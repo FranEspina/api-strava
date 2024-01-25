6e35095f2e0e833aa6a00ee63693674b440db503
Francisco José Espina Aragón <francisco.espina.aragon@gmail.com>
2024-01-25 09:28:24 +0100

    no usar connectDbAsync en modo mock

--- src/db.js
+++ src/db.js
@@ -3,8 +3,11 @@ import { MONGO_CONNSTRING } from './config.js'
 
 export async function connectDbAsync () {
   try{
+    if (process.env.USE_MOCK_DATA_SERVICE === 'yes'){
+      console.log('Mock dataservices : database NOT connected')
+      return 
+    }
     await mongoose.connect(MONGO_CONNSTRING())
-    console.log('database connected')
   } catch (error) {
     console.log(error)
   }
